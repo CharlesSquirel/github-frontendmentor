66997a8da708a7fc32bafb1e57bcf3c083bafd94
CharlesSquirel <karol.wiewiorka@gmail.com>
2024-09-11 18:48:44 +0200

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,20 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        blue: '#0079FF',
+        black: '#2B3442',
+        white: '#fefefe',
+        darkGray: '#697C9A',
+        darkBlue: '#4B6A9B',
+        lightGray: '#F6F8FF',
       },
     },
   },
